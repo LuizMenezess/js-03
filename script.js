075444eae1b2f00086a3803c13197ca4e7a217e5
LuizMenezess <luizmenezes334@gmail.com>
2021-05-14 18:54:45 -0300

    adicionando desafio script.js 03

---
+++ script.js
@@ -0,0 +1,21 @@
+//Criar um loop que conte de 1 até 10 usando FOR
+//Criar um loop que conte de 10 até 1 usando WHILE
+//Criar um loop que conte todos os números ímpares de 1 até 100
+
+//01
+for(let a = 1; a <= 10; a++){
+    console.log(a)
+}
+
+//02
+let c = 10;
+
+while(c >= 1){
+    console.log(c)
+    c--
+}
+
+//03
+for(let b = 1; b <= 100; b += 2){
+    console.log(b)
+}
